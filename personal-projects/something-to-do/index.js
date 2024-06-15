a2b5208beaa88ebb96da3a6296948af771b84726
Marius <mariusionitabogdan@gmail.com>
2024-06-16 01:22:20 +0300

    new update

--- personal-projects/something-to-do/index.js
+++ personal-projects/something-to-do/index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import bodyParser from "body-parser";
-import axios from "axios";
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
