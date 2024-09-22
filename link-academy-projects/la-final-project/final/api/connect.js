9b902a659f7b8ec09135d371e39977d54da04ec3
marius <mariuss.b094@mail.com>
2024-09-22 13:38:11 +0300

    removed project

--- link-academy-projects/la-final-project/final/api/connect.js
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql"
-
-export const db = mysql.createConnection({
-    host: "localhost",
-    user: "mariusbo_root",
-    password: "bB571174000",
-    database: "social"
-});
